1dd72c821b161176b40cc9f967d26906ee45e09e
Emily Pillmore <emilypi@cohomolo.gy>
2020-09-30 22:00:32 -0400

    fix traversals and higher applicatives

--- src/Data/HFunctor.hs
+++ src/Data/HFunctor.hs
@@ -10,19 +10,23 @@
 {-# language TypeOperators #-}
 module Data.HFunctor where
 
-
-import Data.Kind
+import Control.Comonad
 
 -- -------------------------------------------------------------------- --
 -- Data
 
 type f ~> g = forall a. f a -> g a
-type f <~> g = (f ~> g) -> (g ~> f)
+type f <~> g = NIso f g
 
 type NT :: (* -> *) -> (* -> *) -> *
 type role NT nominal nominal
 newtype NT f g where
   NT :: (f ~> g) -> NT f g
+
+type NIso :: (* -> *) -> (* -> *) -> *
+type role NIso nominal nominal
+data NIso f g where
+  NIso :: (f ~> g) -> (g ~> f) -> NIso f g
 
 type HCompose
   :: ((* -> *) -> * -> *)
@@ -45,13 +49,16 @@ newtype HIdentity f where
 -- Classes
 
 class HFunctor t where
+  ffmap :: Functor f => (a -> b) -> t f a -> t f b
   hmap :: (f ~> g) -> (t f ~> t g)
-  {-# minimal hmap #-}
+  {-# minimal hmap, ffmap #-}
 
 class HFunctor t => HApplicative t where
   hpure :: f ~> t f
-  hday :: (forall x. f x -> g x -> h x) -> t f a -> t g a -> t h a
-  {-# minimal hpure, hday #-}
+  happly :: (forall x. f x -> g x -> u f g x) -> t f a -> t g b -> t (u f g) c
+
+  alift2 :: Applicative f => (a -> b -> c) -> t f a -> t f b -> t f c
+  {-# minimal alift2, hpure, happly #-}
 
 class HContravariant t where
   hcontramap :: (f ~> g) -> (t g ~> t f)
@@ -66,34 +73,40 @@ class HDivisible t => HDecideable t where
   {-# minimal hdecide #-}
 
 class HFunctor t => HTraversable t where
+  atraverse :: Applicative h => (forall i. f i -> h (g i)) -> t f a -> h (t g b)
+
   htraverse :: HApplicative u => (forall i. f i -> u g i) -> t f ~> u (t g)
   htraverse f = hsequence . hmap f
 
   hsequence :: HApplicative u => t (u f) ~> u (t f)
   hsequence = htraverse id
-  {-# minimal htraverse | hsequence #-}
+  {-# minimal atraverse, (htraverse | hsequence) #-}
 
 class HFunctor t => HDistributive t where
   hdistribute :: HFunctor u => u (t f) ~> t (u g)
   {-# minimal hdistribute #-}
 
 class HApplicative t => HMonad t where
-  hbind :: t f a -> (f ~> (t g)) -> t g a
+  mbind :: Monad m => t m a -> (a -> m b) -> t m b
+
+  hbind :: t f a -> (f ~> t g) -> t g a
   hbind t f = hjoin (hmap f t)
 
   hjoin :: t (t f) ~> t f
   hjoin t = hbind t id
-  {-# minimal hjoin | hbind #-}
+  {-# minimal mbind, (hjoin | hbind) #-}
 
 class HFunctor t => HComonad t where
   hcopure :: t g ~> g
+
+  cextract :: Comonad w => t w a -> (w a -> b) -> t w b
 
   hcobind :: t g a -> (t g ~> f) -> t f a
   hcobind t f = hmap f (hcojoin t)
 
   hcojoin :: t f ~> t (t f)
   hcojoin t = hcobind t id
-  {-# minimal hcopure, (hcobind | hcojoin) #-}
+  {-# minimal hcopure, cextract, (hcobind | hcojoin) #-}
 
 class (HFunctor l, HFunctor r) => HAdjunction l r where
   hunit :: a ~> r (l a)
@@ -114,5 +127,11 @@ class HFunctor t => HRepresentable t where
   type HRep t :: * -> *
 
   htabulate :: HRep t ~> t f
+  htabulate = case hrepresent of NIso _ g -> g
+
   hindex :: t f ~> HRep t
-  {-# minimal htabulate, hindex #-}
+  hindex = case hrepresent of NIso f _ -> f
+
+  hrepresent :: t f <~> HRep t
+  hrepresent = NIso hindex htabulate
+  {-# minimal (hindex, htabulate) | hrepresent  #-}
